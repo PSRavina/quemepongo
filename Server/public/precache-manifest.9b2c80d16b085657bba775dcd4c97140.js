44d3720e5857d944539caf14fc7a5068f0808dee
Pedro Sanchez <pedro@pop-os.localdomain>
2020-03-05 17:18:23 +0100

    second intenction

---
+++ Server/public/precache-manifest.9b2c80d16b085657bba775dcd4c97140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b96bdc23a5080bb9c2d74435d96b95f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac404954fad4a2cd2e20",
+    "url": "/static/css/main.cc65c12b.chunk.css"
+  },
+  {
+    "revision": "0e396bd50cacfff30873",
+    "url": "/static/js/2.f26a3b24.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.f26a3b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac404954fad4a2cd2e20",
+    "url": "/static/js/main.2a0f9d13.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  }
+]);
