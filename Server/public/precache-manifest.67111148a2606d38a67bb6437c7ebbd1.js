38a49d9f60dcdda13f88b518bb62c992f6c21f71
Pedro Sanchez <pedro@pop-os.localdomain>
2020-03-13 01:06:41 +0100

    antes de heroku

---
+++ Server/public/precache-manifest.67111148a2606d38a67bb6437c7ebbd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ade29261a8f52bba8cace2380c4f269c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3603ee5fda9234b7d83a",
+    "url": "/static/css/main.bf1813f3.chunk.css"
+  },
+  {
+    "revision": "d84a6b1f25606acf8fc7",
+    "url": "/static/js/2.10175233.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.10175233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3603ee5fda9234b7d83a",
+    "url": "/static/js/main.1593e343.chunk.js"
+  },
+  {
+    "revision": "b777e1e5607df564d26b",
+    "url": "/static/js/runtime-main.aaf88d20.js"
+  }
+]);
